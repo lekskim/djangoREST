0b0f9289f3505eb515553feae84e564aed5c904e
lekskim <alek.yakimov@gmail.com>
2023-01-23 01:06:46 +0300

    lesson_04

--- library/library/urls.py
+++ library/library/urls.py
@@ -17,16 +17,19 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from rest_framework.routers import DefaultRouter
-from app.views import AuthorModelViewSet, ArticleModelViewSet, BiographyModelViewSet, BookModelViewSet
+from app.views import AuthorModelViewSet, ArticleModelViewSet, BiographyModelViewSet, BookModelViewSet, MyAPIView
 
 router = DefaultRouter()
 router.register("authors", AuthorModelViewSet)
 router.register("article", ArticleModelViewSet)
 router.register("biography", BiographyModelViewSet)
 router.register("books", BookModelViewSet)
+router.register("my", MyAPIView, basename='my')
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
+    #path("myapi/", MyAPIView.as_view()),
+    path("myapi/", MyAPIView.as_view({'get': 'list'}))
 ]
