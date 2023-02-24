df9a731893faaedbb18d511bfeb163e2d8b2c6f6
lekskim <alek.yakimov@gmail.com>
2023-02-24 21:44:22 +0300

    less 12

--- library/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
-from . import views
-from .views import MyAPIView
-
-app_name = 'authors'
-urlpatterns = [
-    path('', MyAPIView.as_view())
-]
